77bdbdd5abf3257a1ec22a35fb01d4f6fc0e9bd0
zeroplusone <a711186@gmail.com>
2019-06-22 01:01:44 +0800

    Starter code of Homeworks in Lession 4

---
+++ DroidCafe/app/src/main/java/com/example/android/droidcafe/CheckboxesActivity.java
@@ -0,0 +1,17 @@
+package com.example.android.droidcafe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+/**
+ * This activity handles five checkboxes and a Show Toast button for displaying selected checkboxes
+ */
+public class CheckboxesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkboxes);
+    }
+}
